34bb508c42cee272b1540aa79304796042dcca8b
Alex Devlin <alex.devlin@orkes.io>
2026-09-10 11:38:14 -0700

    Fix import path issue

--- webhooks/deploy_wait_for_webhook_workflow.py
+++ webhooks/deploy_wait_for_webhook_workflow.py
@@ -19,9 +19,9 @@ from conductor.ai.agents import AgentRuntime
 
 from settings import settings
 
-from .lib.agent_task import AgentTask
-from .lib.webhook_agent import webhook_agent
-from .lib.workers import get_user_email, send_email
+from .utils.agent_task import AgentTask
+from .utils.webhook_agent import webhook_agent
+from .utils.workers import get_user_email, send_email
 
 WORKFLOW_NAME = "wait_for_webhook_demo"
 WORKFLOW_VERSION = 1
